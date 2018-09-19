2356105cdf7680a7317b3862ab134cdac7c5411f
Nehal <nehal26@gmail.com>
2018-09-19 20:27:03 +0100

    changed to star plus

--- program.py
+++ program.py
@@ -12,7 +12,7 @@ def main():
     for idx, things in enumerate(search_results, 1):
         print(f"{idx}. {things.title}")
 
-    idx_to_view = int(input('Which one would you like to view? '))-1
+    idx_to_view = int(input('Which result would you like to view? '))-1
     full_url = api.get_url(idx_to_view, search_results)
     webbrowser.open(full_url, new=2)
 
